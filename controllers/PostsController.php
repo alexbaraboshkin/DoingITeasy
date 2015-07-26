3321fd9df01afa94eb5261022dfa93de0d230539
alex <alexis1784@gmail.com>
2015-07-26 15:11:10 +0500

    Завершен урок Yii2 Lesson - 6 Access Control http://www.youtube.com/watch?v=Xtz-f_qMIFA

--- controllers/PostsController.php
+++ controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\Posts;
 use app\models\PostsSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -17,6 +18,16 @@ class PostsController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['create','update'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ],
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
